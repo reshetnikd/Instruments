d4619395240c31ca087e9db569eb5387a58d521b
reshetnikd <dmitry.reshetnik@icloud.com>
2020-06-24 19:35:36 +0300

    Refactor code base

--- Instruments/ImageViewController.swift
+++ Instruments/ImageViewController.swift
@@ -49,8 +49,8 @@ class ImageViewController: UIViewController {
         super.viewDidLoad()
 
 		title = image.replacingOccurrences(of: "-Large.jpg", with: "")
-        let path = Bundle.main.path(forResource: image, ofType: nil)!
-		let original = UIImage(contentsOfFile: path)!
+        guard let path = Bundle.main.path(forResource: image, ofType: nil) else { return }
+        guard let original = UIImage(contentsOfFile: path) else { return }
 
 		let renderer = UIGraphicsImageRenderer(size: original.size)
 
--- Instruments/SelectionViewController.swift
+++ Instruments/SelectionViewController.swift
@@ -61,8 +61,8 @@ class SelectionViewController: UITableViewController {
 		// find the image for this cell, and load its thumbnail
 		let currentImage = items[indexPath.row % items.count]
 		let imageRootName = currentImage.replacingOccurrences(of: "Large", with: "Thumb")
-		let path = Bundle.main.path(forResource: imageRootName, ofType: nil)!
-		let original = UIImage(contentsOfFile: path)!
+        guard let path = Bundle.main.path(forResource: imageRootName, ofType: nil) else { return cell}
+        guard let original = UIImage(contentsOfFile: path) else { return cell}
         
         let renderRect = CGRect(origin: .zero, size: CGSize(width: 90, height: 90))
         let renderer = UIGraphicsImageRenderer(size: renderRect.size)
